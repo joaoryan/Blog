07af6e9d76508d73bcab6430aff8f478673564db
joaoryan <sjoaoryan@gmail.com>
2022-08-30 20:54:55 -0300

    feat: add card img

--- my-app/src/pages/Home/index.tsx
+++ my-app/src/pages/Home/index.tsx
@@ -2,17 +2,22 @@ import React, { useEffect, useState } from 'react';
 import axios from '../../services/axios';
 //import { Link } from 'react-router-dom';
 
-//icons
 
-//styles
-import { Section } from './styled';
+
+import tdd from '../../assets/image/testTDD.png'
+import * as Styled from './styled';
 import { Container } from '../../styles/GlobalStyles';
 
 export function Home(): JSX.Element {
 
   return (
-    <Section>
-      HOME HOME
-    </Section>
+    <Styled.Page>
+      <Styled.Card
+        src={tdd}
+        alt='Logo Prática'
+      />
+      
+      
+    </Styled.Page>
   );
 }
--- my-app/src/pages/Home/styled.tsx
+++ my-app/src/pages/Home/styled.tsx
@@ -2,8 +2,15 @@ import styled from 'styled-components';
 import * as color from '../../styles/color';
 
 //geral
-export const Section = styled.div`
+export const Page = styled.div`
   height: 100%;
   width: 100%;
+  padding: 62px;
+`;
+
+export const Card = styled.img`
+  height: 300px;
+  width: 250px;
+  background-color: #fff;
 `;
 
