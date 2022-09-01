990d3903fee9c99a75192c6d88fe827e114be758
joaoryan <sjoaoryan@gmail.com>
2022-09-01 00:22:19 -0300

    feat: add page

---
+++ my-app/src/pages/Cards/TestTDD/index.tsx
@@ -0,0 +1,20 @@
+import React, { useEffect, useState } from 'react';
+
+//import { Link } from 'react-router-dom';
+
+
+
+import tdd from '../../../assets/image/testTDD.png'
+import * as Styled from './styled';
+import { Container } from '../../../styles/GlobalStyles';
+
+export function TestTDD(): JSX.Element {
+
+  return (
+    <Styled.Page>
+      <Styled.Title>TDD</Styled.Title>
+      
+      
+    </Styled.Page>
+  );
+}
