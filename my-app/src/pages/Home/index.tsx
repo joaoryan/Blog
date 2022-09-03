0fa06bb4bdaa4ce164423ce9ccae7368c71fecb2
joaoryan <sjoaoryan@gmail.com>
2022-09-03 16:47:58 -0300

    feat: campo de busca e ajuste em home

--- my-app/src/pages/Home/index.tsx
+++ my-app/src/pages/Home/index.tsx
@@ -8,20 +8,104 @@ import tdd from '../../assets/image/testTDD.png'
 import * as Styled from './styled';
 import { Container } from '../../styles/GlobalStyles';
 import { NavLink } from 'react-router-dom';
+import { BiSearchAlt } from "react-icons/bi";
 
 export function Home(): JSX.Element {
+  const [filteredMenu, setfilteredMenu] = useState<string>('');
 
   return (
     <Styled.Page>
-      <NavLink to='/testTDD' >
-        <Styled.Card
-          src={tdd}
-          alt='Logo Prática'
-        />
-      </NavLink>
-     
-      
-      
+      <Styled.SearchInputDiv>
+        <Styled.IconSearch>
+          <BiSearchAlt />
+        </Styled.IconSearch>
+        <Styled.Input name='busca' placeholder={'Buscar'}
+          onChange={(e) => setfilteredMenu(e.target.value)} />
+      </Styled.SearchInputDiv>
+      <Styled.Cards>
+        <NavLink to='/testTDD' >
+          <Styled.Image
+            src={tdd}
+            alt='Logo Prática'
+          />
+        </NavLink>
+        <NavLink to='/testTDD' >
+          <Styled.Image
+            src={tdd}
+            alt='Logo Prática'
+          />
+        </NavLink>
+        <NavLink to='/testTDD' >
+          <Styled.Image
+            src={tdd}
+            alt='Logo Prática'
+          />
+        </NavLink>
+        <NavLink to='/testTDD' >
+          <Styled.Image
+            src={tdd}
+            alt='Logo Prática'
+          />
+        </NavLink>
+        <NavLink to='/testTDD' >
+          <Styled.Image
+            src={tdd}
+            alt='Logo Prática'
+          />
+        </NavLink>
+        <NavLink to='/testTDD' >
+          <Styled.Image
+            src={tdd}
+            alt='Logo Prática'
+          />
+        </NavLink>
+        <NavLink to='/testTDD' >
+          <Styled.Image
+            src={tdd}
+            alt='Logo Prática'
+          />
+        </NavLink>
+        <NavLink to='/testTDD' >
+          <Styled.Image
+            src={tdd}
+            alt='Logo Prática'
+          />
+        </NavLink>
+        <NavLink to='/testTDD' >
+          <Styled.Image
+            src={tdd}
+            alt='Logo Prática'
+          />
+        </NavLink>
+        <NavLink to='/testTDD' >
+          <Styled.Image
+            src={tdd}
+            alt='Logo Prática'
+          />
+        </NavLink>
+        <NavLink to='/testTDD' >
+          <Styled.Image
+            src={tdd}
+            alt='Logo Prática'
+          />
+        </NavLink>
+        <NavLink to='/testTDD' >
+          <Styled.Image
+            src={tdd}
+            alt='Logo Prática'
+          />
+        </NavLink>
+        <NavLink to='/testTDD' >
+          <Styled.Image
+            src={tdd}
+            alt='Logo Prática'
+          />
+        </NavLink>
+      </Styled.Cards>
+
+
+
+
     </Styled.Page>
   );
 }
--- my-app/src/pages/Home/styled.tsx
+++ my-app/src/pages/Home/styled.tsx
@@ -4,21 +4,65 @@ import * as color from '../../styles/color';
 //geral
 export const Page = styled.div`
   height: 100%;
-  width: 100%;
+  //width: 100vw;
   padding: 62px;
 `;
 
-export const Card = styled.img`
-  height: 270px;
-  width: 220px;
-  background-color: #fff;
-  border-top-left-radius: 25px;
-  transition: 0.5s;
-  :hover{
-    border-top-left-radius: 0px;
-    transition: 0.5s;
-    height: 280px;
-    width: 230px;
+export const SearchInputDiv = styled.div`
+  width: 500px;
+  display: flex;
+  align-items: center;
+`;
+
+export const IconSearch = styled.div`
+  display: flex;
+  align-items: center;
+  position: absolute;
+  svg{
+    font-size: 20px;
+    margin-left: 10px;
+  }
+`
+
+export const Input = styled.input`
+  border: 1px solid ${(props) => props.theme.colors.outlineActiveButton};
+  border-radius: 5px;
+  background: #030518;
+  color: #8899a6;
+  letter-spacing: 1px;
+  padding: 0px 40px;
+  height: 40px;
+  font-size: 12px; 
+  width: 100%;   
+  outline: none;
+  font-weight: bold;
+  :hover {
+    font-style: italic;
   }
 `;
 
+export const Image = styled.img`
+  height: 270px;
+  width: 220px;
+  border-top-left-radius: 25px;
+  transition: 0.5s;
+  margin: 10px;
+  :hover{
+    border-top-left-radius: 0px;
+    transition: 0.5s;
+    height: 275px;
+    width: 225px;
+  }
+`;
+
+export const Cards = styled.div`
+  display: grid;
+  grid-template-columns: auto auto auto auto;
+  margin-top:20px;
+  height: 35rem;
+  overflow-y: scroll;
+  a{
+    display: flex;
+    justify-content: center;
+  }
+`;
